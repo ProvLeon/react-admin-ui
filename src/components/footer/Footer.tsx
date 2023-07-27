1291183ae4fdedeec4a7f14ca179aa9efec1eaa5
ProvLeon <providence.leonard@gmail.com>
2023-07-27 08:48:51 +0100

    update 5 files

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,23 @@
 import "./footer.scss"
 
+const copyrightMessage = () => {
+  const copyright = String.fromCodePoint(169);
+  const year = new Date().getFullYear();
+  const companyName = 'Lama Dev Admin Dashboard';
+
+  return (
+    <>
+      {copyright} {year} {companyName}
+    </>
+  )
+}
+
 const Footer = () => {
   return (
-    <div className="footer">Footer</div>
+    <div className="footer">
+      <span>lamadmin</span>
+      <span>{copyrightMessage()}</span>
+    </div>
   )
 }
 
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,7 +2,26 @@ import "./navbar.scss"
 
 const Navbar = () => {
   return (
-    <div className="navbar">Navbar</div>
+    <div className="navbar">
+      <div className="logo">
+        <img src="logo.svg" alt="logo" />
+        <span>lamadmin</span>
+      </div>
+      <div className="icons">
+        <img src="/search.svg" alt="" className="icon" />
+        <img src="/app.svg" alt="" className="icon" />
+        <img src="expand.svg" alt="" className="icon" />
+        <div className="notification">
+          <img src="/notifications.svg" alt="" />
+          <span>1</span>
+        </div>
+        <div className="user">
+          <img src="https://images.pexels.com/photos/7116213/pexels-photo-7116213.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+          <span>John</span>
+        </div>
+        <img src="/settings.svg" alt="" className="icon" />
+      </div>
+    </div>
   )
 }
 
