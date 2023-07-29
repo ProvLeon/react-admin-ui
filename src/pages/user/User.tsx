ca5eef928636ad0981900e510fa47268f28b6856
ProvLeon <providence.leonard@gmail.com>
2023-07-29 11:49:21 +0100

    update 6 files updated

--- src/pages/user/User.tsx
+++ src/pages/user/User.tsx
@@ -1,13 +1,13 @@
-import Single from "../../components/single/Single"
-import "./user.scss"
-import { singleUser } from "../../data"
+import Single from '../../components/single/Single';
+import './user.scss';
+import { singleUser } from '../../data';
 
 const User = () => {
-  return (
-    <div>
-        <Single {...singleUser}/>
-    </div>
-  )
-}
+    return (
+        <div>
+            <Single {...singleUser} />
+        </div>
+    );
+};
 
-export default User
+export default User;
